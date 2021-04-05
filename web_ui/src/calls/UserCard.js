48e712726489461c36e216506e8d6d2ffe40912d
Angela <hu.ang@northeastern.edu>
2021-04-04 22:20:04 -0400

    cat and button

--- web_ui/src/calls/UserCard.js
+++ web_ui/src/calls/UserCard.js
@@ -1,11 +1,25 @@
 import { Card } from 'react-bootstrap'
 import Visualizer from './Visualizer'
+import ToggleButton from 'react-bootstrap/ToggleButton';
 
 export default function UserCard({user, mediaStream}) {
+    //assuming we have user username?
+    //and the path(?) to profile picture
 
     return (
-        <Card> 
-            <Visualizer mediaStream={mediaStream} />
+        <Card className="text-center" style={{ width: '18rem' }}>
+            <Card.Img src="https://img.webmd.com/dtmcms/live/webmd/consumer_assets/site_images/article_thumbnails/other/cat_relaxing_on_patio_other/1800x1200_cat_relaxing_on_patio_other.jpg?resize=750px:*" alt="Profile Picture" />
+            <Card.ImgOverlay>
+                <Card.Body>
+                    <Card.Header>User's Name</Card.Header>
+                    <Visualizer mediaStream={mediaStream} />
+                    <ToggleButton
+                    type="checkbox"
+                    variant="primary"
+                    value={1}
+                    >Hello</ToggleButton>
+                </Card.Body> 
+            </Card.ImgOverlay>
         </Card>
     )
 
